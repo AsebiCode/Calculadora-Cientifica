d52d96c6bbb28b81443217f24fd10bbc0bf45c05
Isa <kmasi4@gmail.com>
2024-12-01 20:55:46 -0300

    Insercao, AC e Del

--- src/script/javascript.js
+++ src/script/javascript.js
@@ -1,20 +1,28 @@
-// function inserir(num) {
-//     var numero = document.getElementById('resultado').innerHTML
-//     document.getElementById('resultado').innerHTML = numero + num
-// }
+function inserirValores(valor) {
+    var visor = document.getElementsByClassName('visor-insercao')[0]
+    visor.innerHTML += valor
+}
 
-// function apagar() {
-//     document.getElementById('resultado').innerHTML = ""
-// }
+function apagarResultadoDigito() {
+    document.getElementsByClassName('visor-insercao')[0].innerHTML = ""
+    document.getElementsByClassName('visor-resultado')[0].innerHTML = "0"
+}
 
-// function voltar() {
-//     var resultado = document.getElementById('resultado').innerHTML
-//     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
-// }
+function apagarDigito() {
+    var visor = document.getElementsByClassName('visor-insercao')[0]
+    var textoAtual = visor.innerHTML;
+    visor.innerHTML = textoAtual.substring(0, textoAtual.length - 1);
+}
 
-// function calcular() {
-//     var resultado = document.getElementById('resultado').innerHTML
-//     if(resultado) {
-//         document.getElementById('resultado').innerHTML = eval(resultado)
-//     }
-// }
+function calcular() {
+    var visorResultado = document.getElementsByClassName('visor-resultado')[0];
+    var visorInsercao = document.getElementsByClassName('visor-insercao')[0].innerHTML;
+
+    if (visorInsercao) {
+        try {
+            visorResultado.innerHTML = eval(visorInsercao);
+        } catch (e) {
+            visorResultado.innerHTML = "Erro";
+        }
+    }
+}
