018800da364e65b715dea5d79bf8dc578cec1c69
Isa <kmasi4@gmail.com>
2024-12-02 19:38:39 -0300

    correção de botões AC e Del

--- src/script/javascript.js
+++ src/script/javascript.js
@@ -1,28 +1,30 @@
+function visorInsercao() {return document.getElementById("visor-insercao")}
+function visorResultado() {return document.getElementById("visor-resultado")}
+
 function inserirValores(valor) {
-    var visor = document.getElementsByClassName('visor-insercao')[0]
-    visor.innerHTML += valor
+    visorInsercao().innerHTML += valor;
 }
 
 function apagarResultadoDigito() {
-    document.getElementsByClassName('visor-insercao')[0].innerHTML = ""
-    document.getElementsByClassName('visor-resultado')[0].innerHTML = "0"
+    visorInsercao().innerHTML = ""; 
+    visorResultado().innerHTML = "0";
 }
 
 function apagarDigito() {
-    var visor = document.getElementsByClassName('visor-insercao')[0]
-    var textoAtual = visor.innerHTML;
-    visor.innerHTML = textoAtual.substring(0, textoAtual.length - 1);
+    var textoAtual = visorInsercao().innerHTML;
+    visorInsercao().innerHTML = textoAtual.slice(0, -1);
 }
 
 function calcular() {
-    var visorResultado = document.getElementsByClassName('visor-resultado')[0];
-    var visorInsercao = document.getElementsByClassName('visor-insercao')[0].innerHTML;
+    var expressao = visorInsercao().innerHTML;
 
-    if (visorInsercao) {
+    if (expressao) {
         try {
-            visorResultado.innerHTML = eval(visorInsercao);
+            let resultado = eval(expressao);
+            visorResultado().innerHTML = resultado;
+            visorInsercao().innerHTML = "";
         } catch (e) {
-            visorResultado.innerHTML = "Erro";
+            visorResultado().innerHTML = "Erro"; 
         }
     }
 }
