4c410a0c738b0f651d9d65e7e7e47bd359efca4d
Isa <kmasi4@gmail.com>
2024-12-16 18:32:57 -0300

    correções

--- src/script/javascript.js
+++ src/script/javascript.js
@@ -25,21 +25,42 @@ function apagarDigito() {
 
 function normalizarExpressoes(expressao) {
     const substituicoes = [
-        {regex: /x/g, to: '*'}, // multiplicação
-        {regex: /rest/g, to: '%'}, // resto da divisão
-        {regex: /%/g, to: '/100*'}, // porcentagem
-        {regex: /√\s*(\d+)/g, to: 'Math.sqrt($1)'}, // raiz quaquada
-        {regex: /³√\s*(\d+)/g, to: 'Math.cbrt($1)'}, // raiz cúbica
-        {regex: /(\d+)ⁿ√\s*(\d+)/g, to: 'Math.pow($2, 1/$1)'}, // raiz enésima
-        {regex: /(\d+)²/g, to: 'Math.pow($1, 2)'}, // potência ao quadrado
-        {regex: /(\d+)³/g, to: 'Math.pow($1, 3)'}, // potência ao cubo
-        {regex: /(\d+)ⁿ(\d+)/g, to: 'Math.pow($1, $2)'}, // potência genérica
-        {regex: /(\d+)⁻¹/g, to: 'Math.pow($1, -1)'}, // inverso multiplicativo
-        {regex: /(\d+)⁻ⁿ(\d+)/g, to: 'Math.pow($1, -$2)'}, // potência genérica negativa
-        {regex: /(\d+)!/g, to: 'fatorial($1)'}, // fatorial
-        {regex: /(\d+)%\?(\d+)/g, to: '($1 * $2 / 100)'}, // descobrir porcentagem
-        {regex: /log\((\d+),\s*(\d+)\)/g, to: '(Math.log($2) / Math.log($1))'}, // logaritmo comum
-        {regex: /ln\((\d+)\)/g, to: 'Math.log($1)'} // logaritmo natural
+        // Multiplicação (x → *)
+        {regex: /\bx\b/g, to: '*'},
+        // Descobrir porcentagem (x%?y -> x * y / 100)
+        {regex: /(-?\d+(\.\d+)?)%\?(-?\d+(\.\d+)?)/g, to: '($1 * $2 / 100)'},
+        // Porcentagem (% → /100*)
+        {regex: /%/g, to: '/100*'},
+        // Resto da divisão (rest → %)
+        {regex: /rest/g, to: '%'},
+        // Raiz enésima (nⁿ√x)
+        {regex: /(-?\d+(\.\d+)?)ⁿ√\s*(-?\d+(\.\d+)?)/g, to: 'Math.pow($3, 1/$1)'},
+        // Raiz cúbica (³√x)
+        {regex: /³√\s*(-?\d+(\.\d+)?)/g, to: 'Math.cbrt($1)'},
+        // Raiz quadrada (√x)
+        {regex: /√\s*(-?\d+(\.\d+)?)/g, to: 'Math.sqrt($1)'},
+        // Potência genérica (xⁿ)
+        {regex: /(-?\d+(\.\d+)?)ⁿ(-?\d+(\.\d+)?)/g, to: 'Math.pow($1, $3)'},
+        // Potência ao quadrado (x²)
+        {regex: /(-?\d+(\.\d+)?)²/g, to: 'Math.pow($1, 2)'},
+        // Potência ao cubo (x³)
+        {regex: /(-?\d+(\.\d+)?)³/g, to: 'Math.pow($1, 3)'},
+        // Potência com expoente negativo (x⁻ⁿ)
+        {regex: /(-?\d+(\.\d+)?)⁻ⁿ(-?\d+(\.\d+)?)/g, to: 'Math.pow($1, -($3))'},
+        // Inverso multiplicativo (x⁻¹)
+        {regex: /(-?\d+(\.\d+)?)⁻¹/g, to: 'Math.pow($1, -1)'},
+        // Fatorial (x!)
+        {regex: /(-?\d+(\.\d+)?)!/g, to: 'fatorial($1)'},
+        // Logaritmo comum (log base x -> log(y)/log(x))
+        {regex: /log\((-?\d+(\.\d+)?),\s*(-?\d+(\.\d+)?)\)/g, to: '(Math.log($2) / Math.log($1))'},
+        // Logaritmo natural (ln(x))
+        {regex: /ln\((-?\d+(\.\d+)?)\)/g, to: 'Math.log($1)'},
+        // Logaritmo binário (lb(x) -> log base 2)
+        {regex: /lb\((-?\d+(\.\d+)?)\)/g, to: '(Math.log($1) / Math.log(2))'},
+        // Antilogaritmo natural (eˣ)
+        {regex: /e\^\s*(-?\d+(\.\d+)?)/g, to: 'Math.exp($1)'},
+        // Antilogaritmo comum (10ˣ)
+        {regex: /10\^\s*(-?\d+(\.\d+)?)/g, to: 'Math.pow(10, $1)'}
     ];
 
     console.log("Expressão normalizada: ", expressao);
@@ -47,11 +68,43 @@ function normalizarExpressoes(expressao) {
 }
 
 function fatorial(numero) {
+    if (numero % 1 !== 0) {
+        // Para números decimais, é usado a aproximação pela função Gamma
+        return gamma(numero + 1);
+    }
     let resultado = 1;
-    for (let i = numero; i > 0; i--) {
+    for (let i = 1; i <= numero; i++) {
         resultado *= i;
     }
     return resultado;
+}
+
+// Função Gamma para fatorial de decimais
+function gamma(z) {
+    const g = 7;
+    const p = [
+        0.99999999999980993,
+        676.5203681218851,
+        -1259.1392167224028,
+        771.32342877765313,
+        -176.61502916214059,
+        12.507343278686905,
+        -0.13857109526572012,
+        9.9843695780195716e-6,
+        1.5056327351493116e-7
+    ];
+
+    if (z < 0.5) {
+        return Math.PI / (Math.sin(Math.PI * z) * gamma(1 - z));
+    }
+
+    z -= 1;
+    let x = p[0];
+    for (let i = 1; i < g + 2; i++) {
+        x += p[i] / (z + i);
+    }
+    const t = z + g + 0.5;
+    return Math.sqrt(2 * Math.PI) * Math.pow(t, z + 0.5) * Math.exp(-t) * x;
 }
 
 function RemoverPontoInicial() {
